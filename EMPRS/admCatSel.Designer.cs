e1814946449f78df21305349d6a16e762601bf70
Kyle <Kyle@KYLE-PC>
2016-04-19 18:32:43 -0700

    Try again

--- EMPRS/admCatSel.Designer.cs
+++ EMPRS/admCatSel.Designer.cs
@@ -7140,7 +7140,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(494, 24);
+            this.button1.Location = new System.Drawing.Point(715, 27);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 85;
