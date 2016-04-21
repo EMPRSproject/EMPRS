fdf3aed4fb3d99dd758b115d516f7a5627eb6f91
dimalantak <dimalantak@spu.edu>
2016-04-21 15:25:06 -0700

    stsdf

--- EMPRS/studentView.Designer.cs
+++ EMPRS/studentView.Designer.cs
@@ -1075,7 +1075,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(864, 385);
+            this.button1.Location = new System.Drawing.Point(680, 385);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 10;
